bdf0354f8fcb59bb1d7aa444ad3ba5782cd6b215
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-04-01 20:21:30 -0300

    adicionando o arquivo app

---
+++ post-react/src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+import Card from "./components/Card"
+
+export default function App() {
+  return (
+    <Fragment>
+      <Card />
+      
+    </Fragment>
+  )
+}
+
