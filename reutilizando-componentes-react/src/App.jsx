c2717332b7dffea551fb3ff51dfb2abb37617257
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-04-04 22:10:58 -0300

    apredendo a reutilizar componente

---
+++ reutilizando-componentes-react/src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+import Card from "./components/Card"
+
+export default function App() {
+  return (
+    <Fragment>
+      <Card />
+      <Card />
+      <Card />
+    </Fragment>
+  )
+}
+
---
+++ reutilizando-componentes-react/src/components/Card/index.jsx
@@ -0,0 +1,22 @@
+import posterImg from "../../assets/post.jpg"
+
+import styles from "./styles.module.css"
+import Button from "../Button"
+
+ export default function App() {
+   return (
+     <div  className={styles.container}>
+       <div className={styles.containerPoster}>
+         <div className={styles.content}>
+           <img style={{width: "20rem", height: "auto", border: "1rem solid black"}} src={posterImg} alt="" />
+           <div className={styles.texts}>
+             <h1>Pôster: Star Wars (1977)</h1>
+             <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este Clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
+             <Button />
+             <Button />
+           </div>
+         </div>
+       </div>
+     </div>
+   )
+ }
